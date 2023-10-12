8706e046a6125bfb19410e1bb1d5887e7689c2c0
Adrian Lagarto <adrianlagartomc@gmail.com>
2023-10-11 21:09:06 -0700

    yellow glow button

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 function highlightGreyBtn(type){
   const glowGrey = document.querySelector(`${type}`)
     glowGrey.style.backgroundColor = 'rgb(220,220,220)';
@@ -7,11 +6,37 @@ function highlightGreyBtn(type){
     }, 1000)
 }
 
-function highlightNum(num){
+function highlightYellow(sign){
+  const yellow = document.querySelector(`${sign}`)
+  yellow.style.backgroundColor = 'rgb(220,220,220)';
+    setTimeout(function(){
+      yellow.style.backgroundColor = 'rgb(239,154,58)';
+    }, 400)
+}
+
+function highlightNum(num,value){
   const number = document.querySelector(`${num}`)
   number.style.backgroundColor = 'rgb(100,100,100)';
     setTimeout(function(){
       number.style.backgroundColor = 'rgb(50,50,50)';
     }, 400)
+    //^color hightlights when buttons are pressed ^
+
+    const output = document.querySelector('#output');
+    output.innerText = `${value}`
 }
 
+
+const output = document.querySelector('#output');
+
+
+function numberStorage(number){
+  console.log(`${number}`)
+  output.innerText = `${number}`
+}
+
+const button = document.querySelector('.line')
+
+button.addEventListener("click", e =>{
+  console.log();
+})
